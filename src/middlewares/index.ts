f2ab6424b671ab059240588c3faf1beebb00039f
Arjun <kiriti.k999@gmail.com>
2024-10-24 04:17:20 +0530

    fix types

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,8 +1,9 @@
 import { graphqlHTTP } from 'express-graphql';
 import commentRoutes from '../routes/commentRoutes'; // Ensure the correct path to the commentRoutes file
 import { commentGraphQLSchema } from '../graphql/schema'; // Adjust the path as necessary
+import { Application } from 'express';
 
-export const setupCommentingSystem = (app) => {
+export const setupCommentingSystem = (app: Application) => {
     app.use('/api/comments', commentRoutes);
     app.use('/graphql', graphqlHTTP({
       schema: commentGraphQLSchema,
